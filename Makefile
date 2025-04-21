7f248c27a8c973e118b11cbeae6cc5e1de2c2284
kuzan04 <kittiphum1230@hotmail.co.th>
2025-04-21 16:16:30 +0700

    Success version.1

---
+++ Makefile
@@ -0,0 +1,21 @@
+w-install:
+	python -m venv .dataenv
+	call .dataenv\Scripts\activate
+	pip install dotenv pyinstaller
+	
+install:
+	python3 -m venv .dataenv
+	source .dataenv/bin/activate
+	pip install dotenv pyinstaller
+
+w-builds:
+	pyinstaller --onefile --name="search-uri" --icon=icons/myicon.ico --version-file="version.txt" main.py
+builds:
+	pyinstaller --onefile --name="search-uri" --version-file="version.txt" main.py
+	#--icon=icons/myicon.ico
+
+disable:
+	deactivate
+
+dev:
+	python3 main.py
---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+from itertools import groupby
+import tkinter as tk
+import csv
+import ctypes
+from datetime import datetime
+from tkinter import filedialog
+
+
+def select_file(t):
+    # Create the root window & inital window.
+    root = tk.Tk()
+    root.withdraw()
+
+    # Open file about Host Arrary & Check file have or value.
+    try:
+        file = filedialog.askopenfilename(
+            defaultextension=f'.{t}',
+            title="Open File",
+            filetypes=[("Text files", f"*.{t}"), ("All files", "*.*")]
+        )
+    except KeyboardInterrupt:
+        # Delete root window.
+        root.destroy()
+
+    # Convert value in file to array list.
+    if "txt" != t:
+        with open(file, newline='') as f:
+            return list(csv.DictReader(f))
+    else:
+        return open(file, 'r').readlines()
+
+def process(a, b):
+    res = []
+    for i in a:
+        for j in b:
+            if i in j:
+                res.append(f"{i} is match ‼️")
+    return [i for i,_ in groupby(res)]
+
+if __name__ == "__main__":
+    while True:
+        try:
+            print("Select the input file list about URL: ")
+            a = [i.rstrip() for i in select_file("txt")]
+            print("Select the input file CSV from WSA: ")
+            b = [i['Domain or IP'] for i in select_file("csv")]
+            r = process(a,b)
+            print("Result have found in the WSA!!: ")
+            if len(r) != 0:
+                for i in r:
+                    print(i)
+            else:
+                print("Empty not found ❤️")
+            input('press "Enter" to contiunes....')
+        except Exception:
+            sys.exit(0)
